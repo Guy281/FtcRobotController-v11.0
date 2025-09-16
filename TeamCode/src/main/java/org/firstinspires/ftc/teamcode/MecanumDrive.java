51e671465aae89fea4e9e55379f919a97b1be74d
Guy281 <nintendoboi283@gmail.com>
2025-09-15 16:07:02 -1000

    odometry files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -63,14 +63,14 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
         // drive model parameters
-        public double inPerTick = 341.227;
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 0;
+        public double inPerTick = 0.0029306;
+        public double lateralInPerTick = 0.0022557015642490318;
+        public double trackWidthTicks = 4784.874491000018;
 
         // feedforward parameters (in tick units)
-        public double kS = 0.6437986369100956;
-        public double kV = 0.0005515304239898938;
-        public double kA = 0;
+        public double kS = 0.6026898362584934;
+        public double kV = 0.0005639917708751895;
+        public double kA = 0.0001;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,9 +82,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 0;
+        public double lateralGain = 0;
+        public double headingGain = 0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
